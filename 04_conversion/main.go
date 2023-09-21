3bf1596ebad3e8ee4c50d77cd5145453ea906067
MIDLAJ C <mail.midlajc@gmail.com>
2023-09-21 11:41:56 +0530

    day 4 conversion

---
+++ 04_conversion/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	fmt.Println("Welcome to our Pizza App")
+	fmt.Printf("Please rate our pizza between 1 and 5: ")
+
+	reader := bufio.NewReader(os.Stdin)
+
+	rating, _ := reader.ReadString('\n')
+
+	fmt.Println("Thank you for rating our pizza, ", rating)
+
+	numRating, err := strconv.ParseFloat(strings.TrimSpace(rating), 64)
+
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("Added 1 to your rating : ", numRating+1)
+	}
+}
